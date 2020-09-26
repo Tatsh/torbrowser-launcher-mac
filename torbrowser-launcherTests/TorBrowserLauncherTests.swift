ce63c8423aea14ea95e4e1d9e39602d4dc2477b6
Andrew Udvare <audvare@gmail.com>
2020-09-26 01:59:17 -0400

    Fix names of tests

--- torbrowser-launcherTests/TorBrowserLauncherTests.swift
+++ torbrowser-launcherTests/TorBrowserLauncherTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Tor_Browser_Launcher
 
-class torbrowser_launcherTests: XCTestCase {
+class TorBrowserLauncherTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
