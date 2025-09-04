7f8680d056f83c687750449aae5e3217f2652dd1
Andrew Udvare <audvare@gmail.com>
2025-09-04 05:50:21 -0400

    tests: start

--- TorBrowserLauncherTests/TorBrowserLauncherTests.swift
+++ TorBrowserLauncherTests/TorBrowserLauncherTests.swift
@@ -1,19 +1,87 @@
 import XCTest
+import ObjectiveC.runtime
 
 @testable import foo
 
-class TorBrowserLauncherTests: XCTestCase {
+var fileExistsCalled = false
+var removeItemCalled = false
+var shouldFileExist = false
+var shouldThrowOnRemove = false
+
+class MockFileManager: FileManager {
+
+    @objc func mock_fileExists(atPath path: String) -> Bool {
+        fileExistsCalled = true
+        return shouldFileExist
+    }
+
+    @objc func mock_removeItem(at url: URL) throws {
+        removeItemCalled = true
+        if shouldThrowOnRemove {
+            throw NSError(domain: "MockError", code: 1, userInfo: nil)
+        }
+    }
+}
+
+class UtilsRemoveItemIfExistsTests: XCTestCase {
+
+    var mockFileManager: MockFileManager!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        mockFileManager = MockFileManager()
+        swizzleFileManagerMethods()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        unswizzleFileManagerMethods()
+        mockFileManager = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func swizzleFileManagerMethods() {
+        let originalFileExists = class_getInstanceMethod(FileManager.self, #selector(FileManager.fileExists(atPath:)))
+        let mockFileExists = class_getInstanceMethod(MockFileManager.self, #selector(MockFileManager.mock_fileExists(atPath:)))
+        if let originalFileExists = originalFileExists, let mockFileExists = mockFileExists {
+            method_exchangeImplementations(originalFileExists, mockFileExists)
+        }
+
+        let originalRemoveItem = class_getInstanceMethod(FileManager.self, #selector(FileManager.removeItem(at:)))
+        let mockRemoveItem = class_getInstanceMethod(MockFileManager.self, #selector(MockFileManager.mock_removeItem(at:)))
+        if let originalRemoveItem = originalRemoveItem, let mockRemoveItem = mockRemoveItem {
+            method_exchangeImplementations(originalRemoveItem, mockRemoveItem)
+        }
+    }
+
+    func unswizzleFileManagerMethods() {
+        // Swizzling again restores the original implementations
+        swizzleFileManagerMethods()
+    }
+
+    func testRemoveIfExists_FileExists_RemovesItem() {
+        shouldFileExist = true
+
+        removeIfExists(path: "/tmp/testfile")
+
+        XCTAssertTrue(fileExistsCalled)
+        XCTAssertTrue(removeItemCalled)
+    }
+
+    func testRemoveIfExists_FileDoesNotExist_DoesNotRemoveItem() {
+        shouldFileExist = false
+
+        removeIfExists(path: "/tmp/testfile")
+
+        XCTAssertTrue(fileExistsCalled)
+        XCTAssertFalse(removeItemCalled)
+    }
+
+    func testRemoveIfExists_RemoveThrows_PrintsError() {
+        shouldFileExist = true
+        shouldThrowOnRemove = true
+
+        removeIfExists(path: "/tmp/testfile")
+
+        XCTAssertTrue(fileExistsCalled)
+        XCTAssertTrue(removeItemCalled)
     }
 }
+
